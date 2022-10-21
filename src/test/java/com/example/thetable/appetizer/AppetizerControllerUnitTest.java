cc76b990bad5b01f3c3db939ec43e006aca5173a
Christerresin <christer.resin@gmail.com>
2022-10-21 14:25:07 +0200

    add unit test for getAllAppetizers method in AppetizerController

--- src/test/java/com/example/thetable/appetizer/AppetizerControllerUnitTest.java
+++ src/test/java/com/example/thetable/appetizer/AppetizerControllerUnitTest.java
@@ -3,27 +3,40 @@ package com.example.thetable.appetizer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 class AppetizerControllerUnitTest {
+  Appetizer firstAppetizer = new Appetizer("Salmon Surprise", "none", "none", "amazing salmon on a golden plate",
+      "salmon, shellfish");
+
+  Appetizer secondAppetizer = new Appetizer("Salmon Surprise", "none", "none", "amazing salmon on a golden plate",
+      "salmon, shellfish");
+
+  List<Appetizer> actual = Arrays.asList(firstAppetizer, secondAppetizer);
+  List<Appetizer> expected = Arrays.asList(firstAppetizer, secondAppetizer);
 
   @Test
   void shouldReturnAppetizer() {
     AppetizerService appetizerService = Mockito.mock(AppetizerService.class);
-    when(appetizerService.getAppetizer(1)).thenReturn(
-        new Appetizer("Salmon Surprise", "none", "none", "amazing salmon on a golden plate", "salmon, shellfish"));
+    when(appetizerService.getAppetizer(1)).thenReturn(firstAppetizer);
     AppetizerController appetizerController = new AppetizerController(appetizerService);
     assertEquals(200, appetizerController.getAppetizer(1).getStatusCodeValue());
     assertEquals("Salmon Surprise", appetizerController.getAppetizer(1).getBody().getMealTitle());
     assertEquals(
-        new Appetizer("Salmon Surprise", "none", "none", "amazing salmon on a golden plate", "salmon, shellfish"),
+        firstAppetizer,
         appetizerController.getAppetizer(1).getBody());
   }
 
   @Test
   void shouldReturnAppetizersList() {
-
+    AppetizerService appetizerService = Mockito.mock(AppetizerService.class);
+    when(appetizerService.getAllAppetizers()).thenReturn(actual);
+    AppetizerController appetizerController = new AppetizerController(appetizerService);
+    assertEquals(expected, appetizerController.getAllAppetizers());
   }
 
 }
